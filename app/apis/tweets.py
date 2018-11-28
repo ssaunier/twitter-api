08ac1bb943d104e58a3f065568d9a06fc375748c
Sébastien Saunier <seb@saunier.me>
2018-11-28 22:01:01 +0100

    Fix naming conflict with Resource & Model

--- app/apis/tweets.py
+++ app/apis/tweets.py
@@ -1,25 +1,25 @@
 from flask_restplus import Namespace, Resource, fields
 from flask import abort
 from app.db import tweet_repository
-from app.models import Tweet as TweetModel
+from app.models import Tweet
 
 api = Namespace('tweets')
 
-tweet = api.model('Tweet', {
+json_tweet = api.model('Tweet', {
     'id': fields.Integer,
     'text': fields.String,
     'created_at': fields.DateTime
 })
 
-new_tweet = api.model('New tweet', {
+json_new_tweet = api.model('New tweet', {
     'text': fields.String(required=True)
 })
 
 @api.route('/<int:id>')
 @api.response(404, 'Tweet not found')
 @api.param('id', 'The tweet unique identifier')
-class Tweet(Resource):
-    @api.marshal_with(tweet)
+class TweetResource(Resource):
+    @api.marshal_with(json_tweet)
     def get(self, id):
         tweet = tweet_repository.get(id)
         if tweet is None:
@@ -27,8 +27,8 @@ class Tweet(Resource):
         else:
             return tweet
 
-    @api.marshal_with(tweet, code=200)
-    @api.expect(new_tweet, validate=True)
+    @api.marshal_with(json_tweet, code=200)
+    @api.expect(json_new_tweet, validate=True)
     def patch(self, id):
         tweet = tweet_repository.get(id)
         if tweet is None:
@@ -47,13 +47,13 @@ class Tweet(Resource):
 
 @api.route('')
 @api.response(422, 'Invalid tweet')
-class CreateTweet(Resource):
-    @api.marshal_with(tweet, code=201)
-    @api.expect(new_tweet, validate=True)
+class TweetsResource(Resource):
+    @api.marshal_with(json_tweet, code=201)
+    @api.expect(json_new_tweet, validate=True)
     def post(self):
         text = api.payload["text"]
         if len(text) > 0:
-            tweet = TweetModel(text)
+            tweet = Tweet(text)
             tweet_repository.add(tweet)
             return tweet, 201
         else:
