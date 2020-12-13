valerian1978/twitter-api-1

CRUD on tweets with SQLAlchemy

app/apis/tweets.py
@@ -1,7 +1,7 @@
 from flask_restplus import Namespace, Resource, fields
 from flask import abort
-from app.db import tweet_repository
 from app.models import Tweet
+from app import db
 
 api = Namespace('tweets')
 
@@ -21,7 +21,7 @@ json_new_tweet = api.model('New tweet', {
 class TweetResource(Resource):
     @api.marshal_with(json_tweet)
     def get(self, id):
-        tweet = tweet_repository.get(id)
+        tweet = db.session.query(Tweet).get(id)
         if tweet is None:
             api.abort(404, "Tweet {} doesn't exist".format(id))
         else:
@@ -30,7 +30,7 @@ class TweetResource(Resource):
     @api.marshal_with(json_tweet, code=200)
     @api.expect(json_new_tweet, validate=True)
     def patch(self, id):
-        tweet = tweet_repository.get(id)
+        tweet = db.session.query(Tweet).get(id)
         if tweet is None:
             api.abort(404, "Tweet {} doesn't exist".format(id))
         else:
@@ -38,11 +38,12 @@ class TweetResource(Resource):
             return tweet
 
     def delete(self, id):
-        tweet = tweet_repository.get(id)
+        tweet = db.session.query(Tweet).get(id)
         if tweet is None:
             api.abort(404, "Tweet {} doesn't exist".format(id))
         else:
-            tweet_repository.remove(id)
+            db.session.delete(tweet)
+            db.session.commit()
             return None
 
 @api.route('')
@@ -53,8 +54,9 @@ class TweetsResource(Resource):
     def post(self):
         text = api.payload["text"]
         if len(text) > 0:
-            tweet = Tweet(text)
-            tweet_repository.add(tweet)
+            tweet = Tweet(text=text)
+            db.session.add(tweet)
+            db.session.commit()
             return tweet, 201
         else:
             return abort(422, "Tweet text can't be empty")

ssaunier/twitter-api

app/apis/tweets.py
@@ -1,6 +1,7 @@
 from flask_restx import Namespace, Resource, fields
-from app.db import tweet_repository
+from flask import abort
 from app.models import Tweet
+from app import db
 
 api = Namespace('tweets')  # Base route
 
@@ -20,7 +21,7 @@ json_new_tweet = api.model('New tweet', {
 class TweetResource(Resource):
     @api.marshal_with(json_tweet)  # Used to control JSON response format
     def get(self, tweet_id):  # GET method
-        tweet = tweet_repository.get(tweet_id)
+        tweet = db.session.query(Tweet).get(tweet_id)
         if tweet is None:
             api.abort(404)  # abort will throw an exception and break execution flow (equivalent to 'return' keyword for an error)
         return tweet, 200
@@ -28,7 +29,7 @@ class TweetResource(Resource):
     @api.marshal_with(json_tweet, code=200)
     @api.expect(json_new_tweet, validate=True)  # Used to control JSON body format (and validate)
     def patch(self, tweet_id):  # PATCH method
-        tweet = tweet_repository.get(tweet_id)
+        tweet = db.session.query(Tweet).get(tweet_id)
         if tweet is None:
             api.abort(404)
 
@@ -39,10 +40,12 @@ class TweetResource(Resource):
         return None, 204
 
     def delete(self, tweet_id):  # DELETE method
-        tweet = tweet_repository.get(tweet_id)
+        tweet = db.session.query(Tweet).get(tweet_id)
         if tweet is None:
             api.abort(404)
-        tweet_repository.remove(tweet_id)
+
+        db.session.delete(tweet)
+        db.session.commit()
         return None, 204
 
 @api.route('')  # empty route extension (ie: /tweets)
@@ -54,8 +57,9 @@ class TweetsResource(Resource):
         # No need to verify if 'text' is present in body, or if it is a valid string since we use validate=True
         # body has already been validated using json_new_tweet schema
         text = api.payload['text']
-        tweet = Tweet(text)
-        tweet_repository.add(tweet)
+        tweet = Tweet(text=text)
+        db.session.add(tweet)
+        db.session.commit()
         return tweet, 201
 
     # Here we use marshal_list_with (instead of marshal_with) to return a list of tweets